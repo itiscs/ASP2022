1339c5d25f3fcc2924367545ee01b5f8030b4b39
moudroff <moudroff>
2022-03-24 14:18:30 +0300

    First MVC

---
+++ FirstMVC/FirstMVC/Controllers/SecondController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
